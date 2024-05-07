7e669965db0dfc18a867b816730e06e221517b28
Khalid Lawal Abiola <81440160+khalsz@users.noreply.github.com>
2024-05-07 23:24:49 +0100

    fix:db tables population and constraint error

--- database/populate_table.py
+++ database/populate_table.py
@@ -1,18 +1,18 @@
 from sqlalchemy import insert
 from sqlalchemy.orm import  Session
-from database import Time, Product, Customer, Order, Fact
+from database.create_table import Time, Product, Promotion, Sales, Store
 
-def populate_tables(engine, fact_dicts, product_dicts, customer_dicts, time_dicts, order_dicts): 
+def populate_tables(engine, sales_dicts, product_dicts, store_dicts, date_dicts, promotion_dicts): 
     """
     Populates database tables with data.
 
     Args:
         engine (Engine): SQLAlchemy engine object for the database.
-        fact_dicts (list of dict): List of dictionaries containing data for the Fact table.
+        sales_dicts (list of dict): List of dictionaries containing data for the Sales table.
         product_dicts (list of dict): List of dictionaries containing data for the Product table.
-        customer_dicts (list of dict): List of dictionaries containing data for the Customer table.
-        time_dicts (list of dict): List of dictionaries containing data for the Time table.
-        order_dicts (list of dict): List of dictionaries containing data for the Order table.
+        store_dicts (list of dict): List of dictionaries containing data for the Store table.
+        date_dicts (list of dict): List of dictionaries containing data for the Date table.
+        promotion_dicts (list of dict): List of dictionaries containing data for the Promotion table.
 
     Raises:
         Exception: If an error occurs during the data insertion process.
@@ -21,14 +21,14 @@ def populate_tables(engine, fact_dicts, product_dicts, customer_dicts, time_dict
         with Session(engine) as session: 
             # Insert data into Product table
             session.execute(insert(Product), product_dicts)
-            # Insert data into Time table
-            session.execute(insert(Time), time_dicts)
-            # Insert data into Customer table
-            session.execute(insert(Customer), customer_dicts)
-            # Insert data into Order table
-            session.execute(insert(Order), order_dicts)
-            # Insert data into Fact table
-            session.execute(insert(Fact), fact_dicts)
+            # Insert data into Date table
+            session.execute(insert(Time), date_dicts)
+            # Insert data into Store table
+            session.execute(insert(Store), store_dicts)
+            # Insert data into Promotion table
+            session.execute(insert(Promotion), promotion_dicts)
+            # Insert data into Sales table
+            session.execute(insert(Sales), sales_dicts)
             session.commit()
     except Exception as e: 
         raise Exception("Error populating table with data: {e}")
